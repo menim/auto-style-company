f9836b354d2e5b2e7efe7d25d18ca0c1e1682cee
menim <mel198629@gmail.com>
2019-11-25 19:47:35 +0200

    Change functionality of service-button, instead open modal window to scroll to paper-cars section

--- src/components/ServiсeList.vue
+++ src/components/ServiсeList.vue
@@ -1,17 +1,11 @@
 <script>
-import BaseSublist from "./BaseSublist";
-import EventBus from "../eventBus"
+import BaseSublist from './BaseSublist';
 
 export default {
-  name: "ServiceList",
+  name: 'ServiceList',
   components: {
-    BaseSublist
+    BaseSublist,
   },
-  methods: {
-    emitShowModal() {
-      EventBus.$emit('showModal');
-    }
-  }
 };
 </script>
 <template>
@@ -20,9 +14,13 @@ export default {
       <li class="service__item service__item--lg">
         <h3 class="service__subtitle">Оклейка авто под такси по ГОСТУ МО</h3>
         <BaseSublist
-          :sublist="[{content: 'Оклейка в белый цвет'}, {content: 'Оклейка в желтый цвет'}, {content: 'Оклейка гост'}]"
+          :sublist="[
+            {content: 'Оклейка в белый цвет'},
+            {content: 'Оклейка в желтый цвет'},
+            {content: 'Оклейка гост'},
+          ]"
         />
-        <button @click="emitShowModal" class="service__button">Подробнее</button>
+        <a v-scroll-to="'#paper-cars'" class="service__button">Подробнее</a>
       </li>
     </ul>
     <ul class="service__list col-md-6 col-xl-4">
@@ -30,7 +28,9 @@ export default {
         <h3 class="service__subtitle">Оформление путевых листов</h3>
       </li>
       <li class="service__item">
-        <h3 class="service__subtitle">Договор планового предрейсового осмотра</h3>
+        <h3 class="service__subtitle">
+          Договор планового предрейсового осмотра
+        </h3>
       </li>
       <li class="service__item">
         <h3 class="service__subtitle">Оформление карточки водителя</h3>
@@ -71,8 +71,8 @@ export default {
 }
 
 .service__subtitle::before {
-  content: "";
-  background-image: url("../assets/images/checkbox-selected-icon.svg");
+  content: '';
+  background-image: url('../assets/images/checkbox-selected-icon.svg');
   background-repeat: no-repeat;
   flex: 0 0 auto;
   margin-right: 16px;
@@ -81,10 +81,14 @@ export default {
 }
 
 .service__button {
+  cursor: pointer;
   background-color: transparent;
   border: 1px solid #1650fe;
   border-radius: 2px;
   color: #1650fe;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   font-size: 0.75rem;
   text-align: center;
   padding: 0;
